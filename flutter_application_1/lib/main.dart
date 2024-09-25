77cdd4642399f891dedc9527bfd4f9ead932d355
danielajgr <daniela92602@gmail.com>
2024-09-25 17:01:01 -0500

    Revert "adding car class"
    This reverts commit 827324fb7602e25d7fd7daa53ed8aa0c03a928c7.

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,28 +1,101 @@
-// Copyright 2017 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// ignore_for_file: public_member_api_docs
-
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/pages/demo_home.dart';
+import 'game.dart';
+import 'end.dart'; // Import the EndPage
+import 'package:sensors_plus/sensors_plus.dart';
 
+// Found landscape orientation here: https://www.geeksforgeeks.org/flutter-managing-device-orientation/
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Sensors Demo',
+      title: 'Flutter Demo',
+
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 17, 184, 255)),
+        useMaterial3: true,
       ),
-      home: const SensorHomePage(title: 'Sensors Demo'),
+      //Where the pages take you
+      routes: {
+        '/': (context) => const MyHomePage(title: 'Arkansas'),
+        '/game': (context) => const GamePage(), 
+        '/end': (context) => const EndPage(),   
+      },
     );
   }
 }
 
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        
+        title: Center(
+          child: Text(
+            widget.title,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              fontSize: 60, 
+              color: Color.fromARGB(255, 157, 38, 30),
+              fontFamily: 'Times New Roman', 
+              fontWeight: FontWeight.bold, 
+            ),
+          ),
+        ),
+      ),
+      body: Center(
+
+        child: Column(
+          
+          // wireframe for each widget.
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'On the Road',
+              style: TextStyle(
+                fontSize: 80,
+                fontFamily: 'Times New Roman', 
+                fontWeight: FontWeight.bold,
+                ),
+            ),
+            const Text(
+              'Again!!!',
+              style: TextStyle(
+                fontSize: 80,
+                fontFamily: 'Times New Roman', 
+              fontWeight: FontWeight.bold,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // go to game screen
+                Navigator.pushNamed(context, '/game');
+              },
+              child: const Text('Play'), 
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

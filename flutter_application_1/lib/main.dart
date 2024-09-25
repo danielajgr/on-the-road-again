827324fb7602e25d7fd7daa53ed8aa0c03a928c7
danielajgr <daniela92602@gmail.com>
2024-09-25 16:55:09 -0500

    adding car class

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,101 +1,28 @@
-import 'package:flutter/material.dart';
-import 'game.dart';
-import 'end.dart'; // Import the EndPage
-import 'package:sensors_plus/sensors_plus.dart';
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
-// Found landscape orientation here: https://www.geeksforgeeks.org/flutter-managing-device-orientation/
+// ignore_for_file: public_member_api_docs
+
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/pages/demo_home.dart';
+
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-
+      title: 'Sensors Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 17, 184, 255)),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      //Where the pages take you
-      routes: {
-        '/': (context) => const MyHomePage(title: 'Arkansas'),
-        '/game': (context) => const GamePage(), 
-        '/end': (context) => const EndPage(),   
-      },
+      home: const SensorHomePage(title: 'Sensors Demo'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        
-        title: Center(
-          child: Text(
-            widget.title,
-            textAlign: TextAlign.center,
-            style: const TextStyle(
-              fontSize: 60, 
-              color: Color.fromARGB(255, 157, 38, 30),
-              fontFamily: 'Times New Roman', 
-              fontWeight: FontWeight.bold, 
-            ),
-          ),
-        ),
-      ),
-      body: Center(
-
-        child: Column(
-          
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'On the Road',
-              style: TextStyle(
-                fontSize: 80,
-                fontFamily: 'Times New Roman', 
-                fontWeight: FontWeight.bold,
-                ),
-            ),
-            const Text(
-              'Again!!!',
-              style: TextStyle(
-                fontSize: 80,
-                fontFamily: 'Times New Roman', 
-              fontWeight: FontWeight.bold,
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                // go to game screen
-                Navigator.pushNamed(context, '/game');
-              },
-              child: const Text('Play'), 
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
---
+++ flutter_application_1/lib/pages/demo_home.dart
@@ -0,0 +1,120 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/widgets/car.dart';
+import 'package:flutter_application_1/widgets/sensor_display.dart';
+
+import 'package:sensors_plus/sensors_plus.dart';
+
+class SensorHomePage extends StatefulWidget {
+  const SensorHomePage({Key? key, this.title}) : super(key: key);
+
+  final String? title;
+
+  @override
+  SensorHomePageState createState() => SensorHomePageState();
+}
+
+class SensorHomePageState extends State<SensorHomePage> {
+  static const int _snakeRows = 20;
+  static const int _snakeColumns = 20;
+  static const double _snakeCellSize = 10.0;
+
+  List<double>? _accelerometerValues;
+  List<double>? _userAccelerometerValues;
+  List<double>? _gyroscopeValues;
+  List<double>? _magnetometerValues;
+  final _streamSubscriptions = <StreamSubscription<dynamic>>[];
+
+  @override
+  void dispose() {
+    super.dispose();
+    for (final subscription in _streamSubscriptions) {
+      subscription.cancel();
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _streamSubscriptions.add(
+      accelerometerEvents.listen(
+        (AccelerometerEvent event) {
+          setState(() {
+            _accelerometerValues = <double>[event.x, event.y, event.z];
+          });
+        },
+      ),
+    );
+    _streamSubscriptions.add(
+      gyroscopeEvents.listen(
+        (GyroscopeEvent event) {
+          setState(() {
+            _gyroscopeValues = <double>[event.x, event.y, event.z];
+          });
+        },
+      ),
+    );
+    _streamSubscriptions.add(
+      userAccelerometerEvents.listen(
+        (UserAccelerometerEvent event) {
+          setState(() {
+            _userAccelerometerValues = <double>[event.x, event.y, event.z];
+          });
+        },
+      ),
+    );
+    _streamSubscriptions.add(
+      magnetometerEvents.listen(
+        (MagnetometerEvent event) {
+          setState(() {
+            _magnetometerValues = <double>[event.x, event.y, event.z];
+          });
+        },
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final accelerometer =
+        _accelerometerValues?.map((double v) => v.toStringAsFixed(1)).toList();
+    final gyroscope =
+        _gyroscopeValues?.map((double v) => v.toStringAsFixed(1)).toList();
+    final userAccelerometer = _userAccelerometerValues
+        ?.map((double v) => v.toStringAsFixed(1))
+        .toList();
+    final magnetometer =
+        _magnetometerValues?.map((double v) => v.toStringAsFixed(1)).toList();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title!),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Center(
+            child: DecoratedBox(
+              decoration: BoxDecoration(
+                border: Border.all(width: 1.0, color: Colors.black38),
+              ),
+              child: SizedBox(
+                height: _snakeRows * _snakeCellSize,
+                width: _snakeColumns * _snakeCellSize,
+                child: Car(
+                  rows: _snakeRows,
+                  columns: _snakeColumns,
+                  cellSize: _snakeCellSize,
+                ),
+              ),
+            ),
+          ),
+          SensorDisplay(label: "Accelerometer", value: accelerometer),
+          SensorDisplay(label: "UserAccelerometer", value: userAccelerometer),
+          SensorDisplay(label: "Gyroscope", value: gyroscope),
+          SensorDisplay(label: "Magnetometer", value: magnetometer),
+        ],
+      ),
+    );
+  }
+}

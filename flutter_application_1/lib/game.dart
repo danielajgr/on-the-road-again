367e35a8e0520ee6fbcd080a47f0e6a7b5aae5d4
hamjac5334 <50788084+hamjac5334@users.noreply.github.com>
2024-09-25 20:38:01 -0500

    Added yellow lines coming from top and minor details of app

--- flutter_application_1/lib/game.dart
+++ flutter_application_1/lib/game.dart
@@ -1,42 +1,115 @@
 import 'package:flutter/material.dart';
-import 'end.dart';
+import 'dart:async';
 
-class GamePage extends StatelessWidget {
+class GamePage extends StatefulWidget {
   const GamePage({Key? key}) : super(key: key);
+
+  @override
+  _GamePageState createState() => _GamePageState();
+}
+
+class _GamePageState extends State<GamePage> {
+  double lineOffset = 0;
+  Timer? _timer; 
+
+
+//median controller
+  @override
+  void initState() {
+    super.initState();
+    _timer = Timer.periodic(const Duration(milliseconds: 75), (timer) {
+      if (mounted) { 
+        setState(() {
+          lineOffset += 10;
+          if (lineOffset >= 200) {
+            lineOffset = 0; 
+          }
+        });
+      }
+    });
+  }
+
+//handle set state eorr when game restarts
+  @override
+  void dispose() {
+    _timer?.cancel(); 
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Game Page'),
-        automaticallyImplyLeading: false, // get rid of  back button
-      ),
       body: Container(
         color: Colors.green,
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              // road
               Expanded(
                 child: Container(
-                  width: 500, 
-                  color: const Color.fromARGB(255, 68, 68, 68), 
-    
+                  width: 600, 
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 68, 68, 68), 
+                    //add borders
+                    border: Border(
+                      left: BorderSide(width: 10.0, color: Colors.black), 
+                      right: BorderSide(width: 10.0, color: Colors.black),
+                      bottom: BorderSide(width: 10.0, color: Colors.black), 
+                    ),
+                  ),
+
+                  //yellow rects
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        top: lineOffset,
+                        left: 290, // 
+                        child: _buildLine(),
+                      ),
+                      Positioned(
+                        top: lineOffset - 200,
+                        left: 290,
+                        child: _buildLine(),
+                      ),
+                      Positioned(
+                        top: lineOffset - 400,
+                        left: 290, 
+                        child: _buildLine(),
+                      ),
+                      Positioned(
+                        top: lineOffset - 600,
+                        left: 290, 
+                        child: _buildLine(),
+                      ),
+                      Positioned(
+                        top: lineOffset - 800,
+                        left: 290, 
+                        child: _buildLine(),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-              const SizedBox(height: 20), 
-              //test button to go to end.dart
+              const SizedBox(height: 20),
               ElevatedButton(
                 onPressed: () {
                   Navigator.pushNamed(context, '/end');
                 },
-                child: const Text('End'), 
+                child: const Text('End'),
               ),
             ],
           ),
         ),
       ),
+    );
+  }
+
+  // create a yellow median
+  Widget _buildLine() {
+    return Container(
+      width: 20, 
+      height: 100, 
+      color: Colors.yellow,
     );
   }
 }

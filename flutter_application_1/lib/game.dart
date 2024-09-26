efb9c9eab0b0a22d8fa39aa211ca0ad203795036
hamjac5334 <50788084+hamjac5334@users.noreply.github.com>
2024-09-26 16:10:44 -0500

    Got the bushes to work

--- flutter_application_1/lib/game.dart
+++ flutter_application_1/lib/game.dart
@@ -54,9 +54,10 @@ class _GamePageState extends State<GamePage> {
           if (lineOffset >= 325) {
             lineOffset = 0;
           }
-          if (bushOffset >= 525) {
+          if (bushOffset >= 1000) {
             bushOffset = 0;
           }
+          
         });
       }
     });
@@ -83,23 +84,48 @@ class _GamePageState extends State<GamePage> {
 //game scene
   @override
   Widget build(BuildContext context) {
+    startPoints();
     return Scaffold(
       appBar: AppBar(
         title: const Text('Game Page'),
         automaticallyImplyLeading: false, // get rid of back button
       ),
-      body: Container(
-        color: Colors.green,
-        child: Center(
+      body: Stack(
+      children: [
+        Container(color: Colors.green),
+    
+        Positioned(
+          top: bushOffset - 600,
+          left: 40, 
+          child: Circle(),
+        ),
+        Positioned(
+          top: bushOffset - 1200,
+          left: 40, 
+          child: Circle(),
+        ),
+        Positioned(
+          top: bushOffset - 600,
+          left: 800, 
+          child: Circle(),
+        ),
+        Positioned(
+          top: bushOffset - 1200,
+          left: 800, 
+          child: Circle(),
+        ),
+
+
+        Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               // road
               Expanded(
                 child: Container(
-                  width: 600,
-                  decoration: const BoxDecoration(
-                    color: Color.fromARGB(255, 68, 68, 68),
+                  width: 600, 
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 68, 68, 68), 
                     //add borders
                     border: Border(
                       left: BorderSide(width: 10.0, color: Colors.black),
@@ -107,64 +133,42 @@ class _GamePageState extends State<GamePage> {
                       bottom: BorderSide(width: 10.0, color: Colors.black),
                     ),
                   ),
-
-                  //yellow rects
+                  //sort out brackets and parentheses
                   child: Stack(
-                    children: [
-                      SizedBox(
-                        height: _carRows * _carCellSize,
-                        width: _carColumns * _carCellSize,
-                        child: Car(
-                          rows: _carRows,
-                          columns: _carColumns,
-                          cellSize: _carCellSize,
-                          yAxis: _yAxis,
-                        ),
-                      ),
+                    children: [SizedBox(
+            height: _carRows * _carCellSize,
+            width: _carColumns * _carCellSize,
+            child: Car(
+              rows: _carRows,
+              columns: _carColumns,
+              cellSize: _carCellSize,
+              yAxis: _yAxis, 
+            ),
+          ),
                       Positioned(
                         top: lineOffset,
-                        left: 290, //
+                        left: 290, // 
                         child: _buildLine(),
                       ),
                       Positioned(
-                        top: lineOffset - 200,
+                        top: lineOffset - 210,
                         left: 290,
                         child: _buildLine(),
                       ),
                       Positioned(
                         top: lineOffset - 400,
-                        left: 290,
+                        left: 290, 
                         child: _buildLine(),
                       ),
                       Positioned(
                         top: lineOffset - 600,
-                        left: 290,
+                        left: 290, 
                         child: _buildLine(),
                       ),
                       Positioned(
                         top: lineOffset - 800,
-                        left: 290,
+                        left: 290, 
                         child: _buildLine(),
-                      ),
-                      Positioned(
-                        top: bushOffset - 200,
-                        left: 400,
-                        child: Circle(),
-                      ),
-                      Positioned(
-                        top: bushOffset - 400,
-                        left: 400,
-                        child: Circle(),
-                      ),
-                      Positioned(
-                        top: bushOffset - 600,
-                        left: 50,
-                        child: Circle(),
-                      ),
-                      Positioned(
-                        top: bushOffset - 800,
-                        left: 50,
-                        child: Circle(),
                       ),
                     ],
                   ),
@@ -173,30 +177,31 @@ class _GamePageState extends State<GamePage> {
               const SizedBox(height: 20),
               //test button to go to end.dart
               ElevatedButton(
-                //if obstacle.checkHit() = true then end, if not then continue
+                /*
+                if obstacle.checkHit() = true then end, if not then continue
+                */
                 onPressed: () {
                   Navigator.pushNamed(context, '/end', arguments: pointCounter);
                 },
                 child: const Text('End'),
               ),
-              const SizedBox(height: 20),
-              // Display the points below the button
-              Text(
-                'Point Score: $points',
-                style: const TextStyle(fontSize: 24, color: Colors.white),
-              ),
+              const Text(
+                'point score',
+                style: TextStyle(fontSize: 25),
+              )
             ],
           ),
         ),
-      ),
-    );
-  }
+      ],
+    ),
+  );
+}
 
   // create a yellow median
   Widget _buildLine() {
     return Container(
-      width: 20,
-      height: 100,
+      width: 20, 
+      height: 100, 
       color: Colors.yellow,
     );
   }
@@ -206,9 +211,9 @@ class _GamePageState extends State<GamePage> {
     return Container(
       width: 70,
       height: 70,
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         shape: BoxShape.circle,
-        color: Color.fromARGB(255, 35, 59, 35),
+        color: const Color.fromARGB(255, 35, 59, 35),
       ),
     );
   }

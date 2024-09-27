0273485401472bb844e4fe4b8bd16204a655660a
Sadie Lewis <LewisSN@hendrix.edu>
2024-09-26 20:42:18 -0500

    Fixed points and got rid of app bar

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -17,14 +17,15 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
 
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 17, 184, 255)),
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 17, 184, 255)),
         useMaterial3: true,
       ),
       //Where the pages take you
       routes: {
         '/': (context) => const MyHomePage(title: 'Arkansas'),
-        '/game': (context) => const GamePage(), 
-        '/end': (context) => const EndPage(),   
+        '/game': (context) => const GamePage(),
+        '/end': (context) => const EndPage(),
       },
     );
   }
@@ -33,7 +34,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-
   final String title;
 
   @override
@@ -41,75 +41,63 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        
         title: Stack(
           alignment: Alignment.center,
-            children: [
-              Align(
-                  alignment: Alignment.center,
-                  child: 
-
-            Text(
-            widget.title,
-          
-
-            style: const TextStyle(
-              fontSize: 60, 
-              color: Color.fromARGB(255, 157, 38, 30),
-              fontFamily: 'Times New Roman', 
-              fontWeight: FontWeight.bold, 
+          children: [
+            Align(
+              alignment: Alignment.center,
+              child: Text(
+                widget.title,
+                style: const TextStyle(
+                  fontSize: 60,
+                  color: Color.fromARGB(255, 157, 38, 30),
+                  fontFamily: 'Times New Roman',
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
             ),
-          ),
-          ),
-          Positioned(
-            left:10,
-            child: Container(
-              width: 40,
-              height: 40,
-               color: Colors.yellow,
-               child: const Center(
-                child: Row(
+            Positioned(
+              left: 10,
+              child: Container(
+                width: 40,
+                height: 40,
+                color: Colors.yellow,
+                child: const Center(
+                  child: Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
-                      
-                  Text(
-                  '7',
-                  style: TextStyle(
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold,
+                      Text(
+                        '7',
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      RotatedBox(
+                        quarterTurns: 1,
+                        child: Text(
+                          '2024',
+                          style: TextStyle(
+                            fontSize: 11,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                RotatedBox(
-                  quarterTurns: 1, 
-                  child: Text(
-                      '2024',
-                      style: TextStyle(
-                        fontSize: 11,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-  ),
-
-
-                ],
-               ),
-            ),
-  ),)
-  ],
+              ),
+            )
+          ],
         ),
       ),
       body: Center(
-
         child: Column(
-          
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
@@ -117,16 +105,16 @@ class _MyHomePageState extends State<MyHomePage> {
               'On the Road',
               style: TextStyle(
                 fontSize: 70,
-                fontFamily: 'Times New Roman', 
+                fontFamily: 'Times New Roman',
                 fontWeight: FontWeight.bold,
-                ),
+              ),
             ),
             const Text(
               'Again!!!',
               style: TextStyle(
                 fontSize: 70,
-                fontFamily: 'Times New Roman', 
-              fontWeight: FontWeight.bold,
+                fontFamily: 'Times New Roman',
+                fontWeight: FontWeight.bold,
               ),
             ),
             ElevatedButton(
@@ -134,15 +122,16 @@ class _MyHomePageState extends State<MyHomePage> {
                 // go to game screen
                 Navigator.pushNamed(context, '/game');
               },
-              child: const Text('Play'), 
-            ),const Text(
+              child: const Text('Play'),
+            ),
+            const Text(
               'The Natural State',
               style: TextStyle(
                 fontSize: 40,
                 color: Color.fromARGB(255, 157, 38, 30),
-                fontFamily: 'Times New Roman', 
+                fontFamily: 'Times New Roman',
                 fontWeight: FontWeight.bold,
-                ),
+              ),
             )
           ],
         ),
